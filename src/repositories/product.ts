f598d00de931ca26a464498b7b2d1e20def46b45
ArterOhm <aef117722@gmail.com>
2023-11-29 19:47:13 +0700

    orderby desc

--- src/repositories/product.ts
+++ src/repositories/product.ts
@@ -1,6 +1,6 @@
-import {PrismaClient, Product} from "@prisma/client"
-import {IProduct, IProductRepository, IcreateProduct, IupdateProduct} from "."
-import {DATA_PRODUCT_SELECT} from "../const"
+import {PrismaClient, Product} from "@prisma/client";
+import {IProduct, IProductRepository, IcreateProduct, IupdateProduct} from ".";
+import {DATA_PRODUCT_SELECT} from "../const";
 
 export default class ProductRepository implements IProductRepository {
   constructor(private prisma: PrismaClient) {}
@@ -8,17 +8,21 @@ export default class ProductRepository implements IProductRepository {
   public getAllProducts(): Promise<IProduct[]> {
     return this.prisma.product.findMany({
       select: DATA_PRODUCT_SELECT,
-    })
+      orderBy: {createdAt: "desc"},
+    });
   }
 
   public getProductById(id: number): Promise<IProduct> {
     return this.prisma.product.findUniqueOrThrow({
       where: {id},
       select: DATA_PRODUCT_SELECT,
-    })
+    });
   }
 
-  public createProduct(userId: string, createProduct: IcreateProduct): Promise<IProduct> {
+  public createProduct(
+    userId: string,
+    createProduct: IcreateProduct
+  ): Promise<IProduct> {
     return this.prisma.product.create({
       data: {
         ...createProduct,
@@ -27,22 +31,24 @@ export default class ProductRepository implements IProductRepository {
         },
       },
       select: DATA_PRODUCT_SELECT,
-    })
+    });
   }
 
-  public updateProduct(id: number, updateProduct: IupdateProduct): Promise<IProduct> {
+  public updateProduct(
+    id: number,
+    updateProduct: IupdateProduct
+  ): Promise<IProduct> {
     return this.prisma.product.update({
       where: {id},
       data: {...updateProduct},
       select: DATA_PRODUCT_SELECT,
-    })
+    });
   }
 
   public deleteProduct(id: number): Promise<IProduct> {
     return this.prisma.product.delete({
       where: {id},
       select: DATA_PRODUCT_SELECT,
-    })
+    });
   }
-
 }
